ab7731aeca55ee1607281e17d6e8b74a8bd4004a
Marc Evans <marc@redbigfoot.com>
2026-05-18 15:59:44 -0600

    Replace G logo with Earth globe, fix site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'https://usgravity.com',
   integrations: [mdx(), svelte()],
 
   markdown: {
